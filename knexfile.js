fc8770e4952ed438d0b01295fe8c161a01cd5b19
Derrick Holleman <derrickholleman@gmail.com>
2021-11-05 14:27:57 -0500

    prep server to deploy

--- knexfile.js
+++ knexfile.js
@@ -4,6 +4,16 @@ require("dotenv").config(); // npm install dotenv
 const { DATABASE_URL } = process.env;
 const path = require("path"); // path for migrations
 
+const {
+  NODE_ENV = "development",
+  DEVELOPMENT_DATABASE_URL,
+  PRODUCTION_DATABASE_URL,
+} = process.env;
+const URL =
+  NODE_ENV === "production"
+    ? PRODUCTION_DATABASE_URL
+    : DEVELOPMENT_DATABASE_URL;
+
 module.exports = {
   development: {
     client: "postgresql",
