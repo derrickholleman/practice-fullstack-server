a4c034c92e21d3c094962f69e074b61b7c90e9b2
Derrick Holleman <derrickholleman@gmail.com>
2021-11-05 15:09:36 -0500

    added production object

--- knexfile.js
+++ knexfile.js
@@ -3,17 +3,21 @@
 require("dotenv").config(); // npm install dotenv
 const path = require("path"); // path for migrations
 
-const {
-  NODE_ENV = "development",
-  DATABASE_URL
-} = process.env;
-const URL =
-  NODE_ENV === "production"
-    ? DATABASE_URL
-    : DATABASE_URL;
+const { NODE_ENV = "development", DATABASE_URL } = process.env;
+const URL = NODE_ENV === "production" ? DATABASE_URL : DATABASE_URL;
 
 module.exports = {
   development: {
+    client: "postgresql",
+    connection: URL,
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+  },
+  production: {
     client: "postgresql",
     connection: URL,
     migrations: {
